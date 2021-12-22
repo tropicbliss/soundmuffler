54984516c61348cabc43b5b1bfb2af54ef50eb57
tropicbliss <tropicbliss@protonmail.com>
2021-12-22 20:45:51 +0800

    Optimise imports

--- src/main/java/com/tropicbliss/soundmuffler/block/ModBlocks.java
+++ src/main/java/com/tropicbliss/soundmuffler/block/ModBlocks.java
@@ -3,13 +3,10 @@ package com.tropicbliss.soundmuffler.block;
 import com.tropicbliss.soundmuffler.SoundMufflerMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
