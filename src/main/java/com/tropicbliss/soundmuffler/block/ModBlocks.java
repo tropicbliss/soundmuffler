7ca1bdcf5c0d7b1c22f91146988bac87cce093fa
tropicbliss <tropicbliss@protonmail.com>
2021-12-22 20:20:14 +0800

    formatting

--- src/main/java/com/tropicbliss/soundmuffler/block/ModBlocks.java
+++ src/main/java/com/tropicbliss/soundmuffler/block/ModBlocks.java
@@ -3,7 +3,9 @@ package com.tropicbliss.soundmuffler.block;
 import com.tropicbliss.soundmuffler.SoundMufflerMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
@@ -14,8 +16,7 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
   public static final Block SOUND_MUFFLER = new Block(
-      FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WOOL).strength(0.8F, 0.8F)
-          .requiresTool());
+      FabricBlockSettings.copyOf(Blocks.WHITE_WOOL));
 
   public static void registerBlocks() {
     Registry.register(Registry.BLOCK, new Identifier(SoundMufflerMod.MOD_ID, "sound_muffler"),
